9d8324454d732e82e39b254e8b898184c7714525
Felipe <fss2@icomp.ufam.edu.br>
2023-05-20 16:20:49 -0400

    Added icon launcher and layouts

---
+++ QuadrinhosChibatas/app/src/main/java/com/irohouse/chibatascomics/ListComics.kt
@@ -0,0 +1,11 @@
+package com.irohouse.chibatascomics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListComics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_comics)
+    }
+}
